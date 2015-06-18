14e61781e5cff8b4215a4ee3ba52850e45b5c0e2
mohanbabu-raju <mohanbabu.raju@gmail.com>
2015-06-18 17:20:56 +0530

    RPN functionality Added.

--- src/test/java/com/training/example/RPNTest.java
+++ src/test/java/com/training/example/RPNTest.java
@@ -1,88 +1,41 @@
 package com.training.example;
 
-import junit.framework.TestCase;
-
 import com.training.rpn.RPNValidate;
 
+import junit.framework.TestCase;
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class RPNTest extends TestCase{
 	
-	String inputA="";
-	String inputB="";
-	String inputC="";
+String input = "";
+String actual = "";
+String condition = "";
+@Given("^input is \"([^\"]*)\"$")
+public void input_is(String arg1) throws Throwable {
+    // Express the Regexp above with the code you wish you had
+    //throw new PendingException();
+	this.input= arg1;
+}
+
+@When("^data is \"([^\"]*)\"$")
+public void data_is(String arg1) throws Throwable {
+    // Express the Regexp above with the code you wish you had
+    //throw new PendingException();
+	this.condition=arg1;
 	
-	
-	
-	@Given("^when inputA is \"([^\"]*)\"$")
-	public void when_inputA_is(String arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	  //  throw new PendingException();
-		inputA = arg1;
-	}
+}
 
-	@When("^data is validated$")
-	public void data_is_validated() throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	   // throw new PendingException();
-	}
+@Then("^result \"([^\"]*)\" is returned$")
+public void result_is_returned(String arg1) throws Throwable {
+    // Express the Regexp above with the code you wish you had
+    //throw new PendingException();
+	RPNValidate rpn = new RPNValidate(input);
+	actual = rpn.checValues(this.input);
+		 
+	assertEquals(String.valueOf(arg1), String.valueOf(actual));
+}
 
-	@Then("^error \"([^\"]*)\" is returned.$")
-	public void error_is_returned(String arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	  //  throw new PendingException();
-		RPNValidate validate = new RPNValidate(inputA);
-		//Check for delimiter
-		boolean statusFlag = validate.checkValidInputData();
-		assertEquals(arg1, String.valueOf(statusFlag));
-	}
-
-	@Given("^when inputB is \"([^\"]*)\"$")
-	public void when_inputB_is(String arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	   // throw new PendingException();
-		inputB = arg1;
-	}
-
-	@Then("^message \"([^\"]*)\" is returned.$")
-	public void message_is_returned(String arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	   // throw new PendingException();
-		RPNValidate validate = new RPNValidate(inputB);
-		//Check for delimiter
-		boolean statusFlag = validate.checkValidInputData();
-		assertEquals(arg1, String.valueOf(statusFlag));
-	}
-
-	@Given("^when inputC is \"([^\"]*)\"$")
-	public void when_inputC_is(String arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	   // throw new PendingException();
-		inputC = arg1;
-	}
-
-	@Then("^result -(\\d+) is returned.$")
-	public void result_is_returned(int arg1) throws Throwable {
-	    // Express the Regexp above with the code you wish you had
-	   // throw new PendingException();
-		RPNValidate validate = new RPNValidate(inputC);
-		int result = validate.calculateValues();
-		System.out.println("expected::"+arg1);
-		System.out.println("actual::"+result);
-		assertEquals(arg1, String.valueOf(result));
-	}
-	
-	@Then("^result \"([^\"]*)\" is returned.$")
-	public void result_is_returned(String arg1) throws Throwable {
-		RPNValidate validate = new RPNValidate(inputC);
-		int result = validate.calculateValues();
-		System.out.println("expected::"+arg1);
-		System.out.println("actual::"+result);
-		assertEquals(arg1, String.valueOf(result));
-	}
-
-
-	
 }
